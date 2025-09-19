51c9ea7809cb014f0d6bb019c477a50064bb3be0
Kurosakiniel <ichigoniel@gmail.com>
2025-09-19 14:36:55 -0300

    Tela de detalhes e avaliações

--- core/views.py
+++ core/views.py
@@ -10,7 +10,6 @@ def lista_roms(request):
     # Renderiza o template passando as ROMs com suas médias
     return render(request, 'core/home.html', {'roms': roms}) 
 
-
 def avaliar_rom(request, rom_id):
     # pega a ROM pelo ID ou retorna 404 se não existir
     rom = get_object_or_404(ROM, id=rom_id)
@@ -33,3 +32,19 @@ def avaliar_rom(request, rom_id):
 def detalhe_rom(request, rom_id):
     rom = get_object_or_404(ROM, id=rom_id)
     return render(request, "core/detalhe_rom.html", {"rom": rom})
+
+def excluir_avaliacao(request, rom_id):
+    if request.method == 'POST':
+        rom = get_object_or_404(ROM, id=rom_id)
+        session_id = request.session.session_key
+        if session_id:
+            try:
+                avaliacao = Avaliacao.objects.get(rom=rom, session_id=session_id)
+                avaliacao.delete()
+                # Atualiza a média
+                media = rom.avaliacoes.aggregate(media=Avg('estrelas'))['media'] or 0
+                return JsonResponse({'media': round(media, 2), 'excluida': True})
+            except Avaliacao.DoesNotExist:
+                return JsonResponse({'erro': 'Avaliação não encontrada'}, status=404)
+        return JsonResponse({'erro': 'Sessão não encontrada'}, status=400)
+    return JsonResponse({'erro': 'Método inválido'}, status=405)
