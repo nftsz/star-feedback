7437c0253ab80f19171ff1d139b800077b8e2a1f
Naftaly Souza <naft.dev@proton.me>
2025-09-19 14:07:20 -0300

    feat: exibir ROMs com imagem default ou definida
    - Implementado novo model
    - Adicionada nova view para mostrar ROMs
    - Ajuste na lógica de exibição de imagens (default ou definida)

--- core/admin.py
+++ core/admin.py
@@ -3,4 +3,5 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.ROM)
+admin.site.register(models.ImagemJogo)
 admin.site.register(models.Avaliacao)
--- core/views.py
+++ core/views.py
@@ -1,31 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from .models import *
+from .models import ROM, Avaliacao
 from django.db.models import Avg
 from django.http import JsonResponse
 
-# Create your views here.
 def lista_roms(request):
-    # pega todas as ROMs e adiciona um campo 'media' com a média das avaliações
-    roms = ROM.objects.all().annotate(media=Avg('avaliacoes__estrelas'))
-    # Renderiza o template passando as ROMs com suas médias
-    return render(request, 'core/home.html', {'roms': roms}) 
+    # pega todas as ROMs com média de avaliação e pré-carrega imagens relacionadas
+    roms = (ROM.objects.all().annotate(media=Avg('avaliacoes__estrelas')).prefetch_related('imagens').order_by('-media')[:5])
+    roms_recent = (ROM.objects.all().annotate(media=Avg('avaliacoes__estrelas')).prefetch_related('imagens').order_by('-criado_em')[:5])
+
+    # transforma a média em int arredondado pra facilitar mostrar estrelas
+    for rom in roms:
+        rom.media_int = int(round(rom.media or 0))
+    for rom in roms_recent:
+        rom.media_int = int(round(rom.media or 0))
+
+    return render(request, 'core/home.html', {'roms': roms, 'roms_recent': roms_recent})
 
 
 def avaliar_rom(request, rom_id):
-    # pega a ROM pelo ID ou retorna 404 se não existir
+    # pega a ROM pelo ID ou retorna 404
     rom = get_object_or_404(ROM, id=rom_id)
-    # garante que a sessão do usuário exista
     session_id = request.session.session_key or request.session.create()
 
     if request.method == 'POST':
         estrelas = int(request.POST.get('estrelas', 0))
         if 1 <= estrelas <= 5:
-            avaliacao, created = Avaliacao.objects.update_or_create(
-                rom=rom, session_id=session_id,
-                defaults={'estrelas': estrelas}
-            )
-            # calcula a média atualizada das estrelas da ROM
+            avaliacao, created = Avaliacao.objects.update_or_create(rom=rom, session_id=session_id,defaults={'estrelas': estrelas})
             media = rom.avaliacoes.aggregate(media=Avg('estrelas'))['media']
-            # retorna a média e se foi criação ou atualização
+
             return JsonResponse({'media': round(media, 2), 'nova': created})
+        
     return JsonResponse({'erro': 'Dados inválidos'}, status=400)
